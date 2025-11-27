dbf442e2c00c2c8d9e36ddb22e111b10025856a3
Jujujulex <ishiwuji@gmail.com>
2025-11-28 00:15:17 +0100

    feat: integrate toast provider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { headers } from "next/headers";
 import ContextProvider from "@/context";
+import { ToastProvider } from "@/context/ToastContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,9 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ContextProvider cookies={cookies}>{children}</ContextProvider>
+        <ContextProvider cookies={cookies}>
+          <ToastProvider>{children}</ToastProvider>
+        </ContextProvider>
       </body>
     </html>
   );
