94ec495ec8397819fd070b446dbb13524886d7e6
Jujujulex <ishiwuji@gmail.com>
2025-11-28 14:02:53 +0100

    fix: add non-null assertion for metadataUrl

--- src/components/NFTActions.tsx
+++ src/components/NFTActions.tsx
@@ -64,7 +64,7 @@ export default function NFTActions({ tokenId, contractAddress, chainId, metadata
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                 </svg>
             ),
-            onClick: () => window.open(metadataUrl, '_blank'),
+            onClick: () => window.open(metadataUrl!, '_blank'),
         }] : []),
     ];
 
